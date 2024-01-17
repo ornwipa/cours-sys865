8139e29b4664dab30632d976d469104b31d15947
ornwipa <fahornwipa@gmail.com>
2024-01-17 16:36:29 -0500

    TP1, fichiers pour covariance et correlation

---
+++ SYS865_Hiver2024_TP1.R
@@ -0,0 +1,23 @@
+# Exemple du TP1, 17 janvier 2024
+data <- read.csv("diabetes.csv")
+moyenne1 <- mean(data$Pregnancies)
+variance1 <- var(data$Pregnancies)
+
+hist(data$Pregnancies)
+for (col in 2:ncol(data)) {
+  fig_name=paste("histogramme/Histogramme", 
+                 colnames(data)[col], ".jpg", 
+                 sep = "")
+  jpeg(file=fig_name)
+  hist(data[,col], 
+       main=paste("Histogramme du paramètre", 
+                  colnames(data)[col]),  
+       xlab=colnames(data)[col])
+  dev.off()
+}
+
+covariances <- round(var(data),3)
+correlations <- round(cor(data),3)
+
+write.csv(covariances, "covariances.csv")
+write.csv(correlations, "correlations.csv")
